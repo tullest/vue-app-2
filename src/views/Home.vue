dce0e84a1d61e4dc32de83ce85434e20a0d88d47
Nicolas Venard <nicolasvenard@mac-mini-de-nicolas.home>
2021-03-09 10:46:07 +0100

    ajout apexcharts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWithName msg="Nicolas Venard"/>
-    <Footer/>
+    <h1>BTC PREDICT PRICE GAME</h1>
+
+    <div id="chart">
+      <VueApexCharts type="area" height="350" :options="chartOptions" :series="series"/>
+    </div>
   </div>
 </template>
 
@@ -10,12 +12,78 @@
 // @ is an alias to /src
 import HelloWithName from '@/components/HelloWithName.vue'
 import Footer from "@/components/Footer";
+import VueApexCharts from "apexcharts";
 
 export default {
   name: 'Home',
   components: {
     HelloWithName,
-    Footer
+    Footer,
+    VueApexCharts,
+  },
+  data: function() {
+    return {
+      series: [{
+        name: 'XYZ MOTORS',
+        data: [34, 25, 48, 124, 18, 39, 48, 100]
+      }],
+      chartOptions: {
+        chart: {
+          type: 'area',
+          stacked: false,
+          height: 350,
+          zoom: {
+            type: 'x',
+            enabled: true,
+            autoScaleYaxis: true
+          },
+          toolbar: {
+            autoSelected: 'zoom'
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        markers: {
+          size: 0,
+        },
+        title: {
+          text: 'Stock Price Movement',
+          align: 'left'
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shadeIntensity: 1,
+            inverseColors: false,
+            opacityFrom: 0.5,
+            opacityTo: 0,
+            stops: [0, 90, 100]
+          },
+        },
+        yaxis: {
+          labels: {
+            formatter: function (val) {
+              return (val / 1000000).toFixed(0);
+            },
+          },
+          title: {
+            text: 'Price'
+          },
+        },
+        xaxis: {
+          type: 'datetime',
+        },
+        tooltip: {
+          shared: false,
+          y: {
+            formatter: function (val) {
+              return (val / 1000000).toFixed(0)
+            }
+          }
+        }
+      },
+    }
   }
 }
 </script>
